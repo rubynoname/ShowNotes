1ddff32ac7356848cd53edb29a566fc7af90de46
Ivan Evtukhovich <evtuhovich@gmail.com>
2012-04-24 13:33:34 +0400

    Поправил все эпизоды для автогенерации

--- rakefile.rb
+++ rakefile.rb
@@ -2,6 +2,6 @@ require 'bluecloth'
 
 task :default do
   file = Dir['S*.md'].sort.last
-  bc = BlueCloth.new File.read(file)
+  bc = BlueCloth.new File.read(file) + File.read('footer.md')
   puts bc.to_html
 end
